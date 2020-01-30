729b2050ad95bdb56e665798fe4f43d3d249395b
ayrbox <srdx7@hotmail.com>
2020-01-30 13:53:07 +0000

    Add grand total of total with updated gitignore

--- src/grandTotal.js
+++ src/grandTotal.js
@@ -11,13 +11,22 @@ function grandTotal(rowIndexes, startAt) {
         const columnIdx = idx + 1;
         const col = COLUMNS[columnIdx];
 
-        const f = rowIndexes.map((rowIdx) => {
-            return `${col}${rowIdx}`;
-        });
+        const f = rowIndexes.map((rowIdx) => `${col}${rowIdx}`);
 
         worksheet.cell(startAt, idx + 2)
             .formula(f.join('+'));
     });
+
+    const TOTAL_COLUMN = 1 + items.length + 1;
+
+
+
+
+    // Grand Total of Total Column
+    const totalCol = COLUMNS[TOTAL_COLUMN - 1];
+    const f = rowIndexes.map((rowIdx) => `${totalCol}${rowIdx}`);
+    worksheet.cell(startAt, TOTAL_COLUMN)
+      .formula(f.join('+'));
 } 
 
 module.exports = grandTotal;
