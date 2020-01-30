e7f6f141cb1d0827ab0b88db4fd8bee6721406a7
ayrbox <srdx7@hotmail.com>
2020-01-30 09:38:01 +0000

    Add daybook items

--- src/expense-items.js
+++ src/expense-items.js
@@ -1,10 +1,34 @@
 module.exports = [{
+    name: 'Earning',
+    expense: false,
+}, {
+    name: 'Diseal',
+    expense:  true,
+}, {
+    name: 'Office Rent',
+    expense:  true,
+}, {
+    name: 'Mobile',
+    expense:  true,
+}, {
     name: 'Insurance',
     expense: true,
 }, {
-    name: 'Income',
-    expense:  false,
+    name: 'Car Rent',
+    expense: true,
 }, {
-    name: 'Income',
-    expense:  false,
+    name: 'Car Rent',
+    expense: true,
+}, {
+    name: 'Service',
+    expense: true,
+}, { 
+    name: 'MOT',
+    expense: true,
+}, {
+    name: 'Road Tax',
+    expense: true,
+}, {
+    name: 'Other Expenses',
+    expense: true,
 }]
