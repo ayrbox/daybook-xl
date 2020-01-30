78931427349c1864cf5f588cf12ea5f9653bfbba
ayrbox <srdx7@hotmail.com>
2020-01-30 13:43:01 +0000

    Add daily total

--- src/generateMonthlySheet.js
+++ src/generateMonthlySheet.js
@@ -25,8 +25,20 @@ var totalStyle = workbook.createStyle({
     }
 })
 
-function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
 
+const DATE_COLUMN = 1; 
+const TOTAL_COLUMN = DATE_COLUMN + items.length + 1;
+
+
+const dailyTotalFormula = (rowIndex, columnNames) => items.map(({ expense }, idx) => {
+  const columnIdx = idx + 1;
+  const col = columnNames[columnIdx];
+  return `${(expense ? '-': '+')}${col}${rowIndex}`
+}).join('');
+
+
+
+function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
     const headerRowIndex = rowIndex;
 
     // Month Name
@@ -42,6 +54,12 @@ function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
             .style(headerStyle);
     });
 
+    // Total Column
+    worksheet
+      .cell(headerRowIndex, TOTAL_COLUMN)
+      .string('Total')
+      .style(headerStyle);
+
     const startDate = Date.UTC(yearIndex, monthIndex, 1);
     const endDate = Date.UTC(yearIndex, monthIndex + 1, 0); // get correct end date
 
@@ -51,10 +69,16 @@ function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
     // Month Date 
     let rowIdx = headerRowIndex;
     while(end >= start) {
-        rowIdx += 1;
-        worksheet.cell(rowIdx, 1)
-            .date(start);
-        start.setDate(start.getDate() + 1);
+      rowIdx += 1;
+      worksheet.cell(rowIdx, 1)
+        .date(start)
+
+      const f = dailyTotalFormula(rowIdx, COLUMNS);
+      worksheet.cell(rowIdx, TOTAL_COLUMN)
+        .formula(f)
+
+      
+      start.setDate(start.getDate() + 1);
     }
 
     // Monthly Sub Total
@@ -64,6 +88,8 @@ function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
         .string('Monthly Total')
         .style(totalStyle);
 
+
+    // Total
     items.forEach((_, idx) => {
         const columnIdx = idx + 1; // First column date
         const col = COLUMNS[columnIdx];
@@ -71,6 +97,12 @@ function generateMonthlySheet(monthIndex, yearIndex, rowIndex = 1) {
             .formula(`SUM(${col}${headerRowIndex+1}:${col}${rowIdx})`)
             .style(totalStyle)
     });
+
+    // Total of Total Column
+    const totalCol = COLUMNS[TOTAL_COLUMN - 1];
+    worksheet.cell(totalRowIdx, TOTAL_COLUMN)
+      .formula(`SUM(${totalCol}${headerRowIndex+1}:${totalCol}${rowIdx})`)
+      .style(totalStyle);
 
     return totalRowIdx; // Last Row of month
 };
